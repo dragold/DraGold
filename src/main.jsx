e3570e076c85c4ba32670c4e6a6276eff4ed176c
Erm <er.malali91@gmail.com>
2026-08-05 20:33:42 +0200

    Refactor main.jsx to use createElement and conditional rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
+import { StrictMode, createElement as h } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
 import DraGold from './DraGold.jsx'
+import CardPage from './pages/card/CardPage.jsx'
+
+const cardMatch = window.location.pathname.match(/^\/carta\/([^/]+)\/?$/)
+const RootView = cardMatch ? h(CardPage, { slug: decodeURIComponent(cardMatch[1]) }) : h(DraGold, null)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <DraGold />
-    <Analytics />
-  </StrictMode>,
-)
+  h(StrictMode, null, RootView, h(Analytics, null))
+  )
