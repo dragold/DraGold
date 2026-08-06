3755b88d78c66f5356a26fe7372275ee21a76e95
Erm <er.malali91@gmail.com>
2026-08-06 13:08:44 +0200

    refactor(search): add lib/searchData.js

---
+++ src/lib/searchData.js
@@ -0,0 +1,16 @@
+export const LANG_ALIASES = {
+  jp:'ja', jpn:'ja', jap:'ja', japanese:'ja',
+  eng:'en', english:'en',
+  ita:'it', italian:'it', italiano:'it',
+  esp:'es', spa:'es', spanish:'es', espanol:'es',
+  por:'pt', bra:'pt', portuguese:'pt', portugues:'pt',
+  ind:'id', indonesian:'id', indonesia:'id',
+  kor:'ko', kr:'ko', korean:'ko',
+  fra:'fr', fre:'fr', french:'fr', francais:'fr',
+  deu:'de', ger:'de', german:'de', deutsch:'de',
+};
+export const RARITY_TOKENS = new Set(['rare','holo','secret','common','uncommon','promo','ultra',
+  'hyper','rainbow','full','illustration','art','trainer','double','amazing','radiant',
+  'shiny','vmax','vstar','vunion','gold','platinum','mythic','epic','legend','super',
+  'special','classic','collection','alternate','foil','parallel','prism']);
+export const JP_NAME_ALIASES = {"charizard":["リザードン"],"pikachu":["ピカチュウ"],"mewtwo":["ミュウツー"],"rayquaza":["レックウザ"],"bulbasaur":["フシギダネ"],"ivysaur":["フシギソウ"],"venusaur":["フシギバナ"],"charmander":["ヒトカゲ"],"charmeleon":["リザード"],"squirtle":["ゼニガメ"],"wartortle":["カメール"],"blastoise":["カメックス"],"raichu":["ライチュウ"],"clefairy":["ピッピ"],"vulpix":["ロコン"],"ninetales":["キュウコン"],"jigglypuff":["プリン"],"gastly":["ゴース"],"haunter":["ゴースト"],"gengar":["ゲンガー"],"onix":["イワーク"],"eevee":["イーブイ"],"vaporeon":["シャワーズ"],"jolteon":["サンダース"],"flareon":["ブースター"],"espeon":["エーフィ"],"umbreon":["ブラッキー"],"leafeon":["リーフィア"],"glaceon":["グレイシア"],"sylveon":["ニンフィア"],"snorlax":["カビゴン"],"dratini":["ミニリュウ"],"dragonair":["ハクリュー"],"dragonite":["カイリュー"],"mew":["ミュウ"],"lugia":["ルギア"],"celebi":["セレビィ"],"gyarados":["ギャラドス"],"lapras":["ラプラス"],"alakazam":["フーディン"],"gardevoir":["サーナイト"],"lucario":["ルカリオ"],"garchomp":["ガブリアス"],"greninja":["ゲッコウガ"],"tyranitar":["バンギラス"],"metagross":["メタグロス"],"salamence":["ボーマンダ"],"darkrai":["ダークライ"],"arceus":["アルセウス"],"palkia":["パルキア"],"dialga":["ディアルガ"],"giratina":["ギラティナ"],"reshiram":["レシラム"],"zekrom":["ゼクロム"],"kyurem":["キュレム"],"luffy":["ルフィ"],"zoro":["ゾロ"],"nami":["ナミ"],"sanji":["サンジ"],"chopper":["チョッパー"],"robin":["ロビン"],"franky":["フランキー"],"brook":["ブルック"],"ace":["エース"],"shanks":["シャンクス"]}
